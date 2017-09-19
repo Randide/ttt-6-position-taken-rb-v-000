81300f21be6f870a55502f0f5715362dcd9b4196
Randi Nash <randidena@live.com>
2017-09-19 01:03:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
 def position_taken?(board, index)
-  if board[index] == "" || board[index] == " " || board[index] = nil
+  if board[index] == "" || board[index] == " " || board[index] == nil
     return false
   end
 end# code your #position_taken? method here!
